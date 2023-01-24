271805bb0c5b0d73ccd1c1ffd60473f3358a1e85
Anderson <andersoncsilva850@gmail.com>
2023-01-24 20:33:32 -0300

    Css e JS
    Estilizando a página de login.
    Criando uma animação da img.
    Implementado função na pagina usando o JS. E colocando uma nova função para prosseguir para a próxima pagina do game

---
+++ js/login.js
@@ -0,0 +1,29 @@
+//1º colocando função no button de habilitar e desabilitar
+
+const input = document.querySelector('.login__input');
+const button = document.querySelector('.login__button');
+
+// declarando função nos formularios
+const form = document.querySelector('.login__form');
+
+const validateInput = ({target}) => {
+    if (target.value.length > 2){
+        button.removeAttribute('disabled');
+        return
+    }
+        button.setAttribute('disabled', ' ');
+}
+
+const handleSubmit = (event) =>{
+    event.preventDefault();
+
+    //salva os dados input no navegador
+    localStorage.setItem('player', input.value);
+    // ir para a proximas paginas!!
+    window.location='pages/game.html'
+}
+
+input.addEventListener('input', validateInput);
+
+// 2ºfunção nos formularios
+form.addEventListener('submit', handleSubmit);
