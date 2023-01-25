f92251f8e91c48567e778b6a069630ffcdd3779d
Anderson <andersoncsilva850@gmail.com>
2023-01-25 11:02:37 -0300

    Adicionado check de jogo finalizado, revelendo cartas e colocando um time

--- js/game.js
+++ js/game.js
@@ -13,10 +13,69 @@ const characters = [
     'rick',
 ];
 
-const createElement =(tag, className) => {
+const createElement = (tag, className) => {
     const element = document.createElement(tag);
     element.className = className;
     return element;
+}
+
+let firstCard = '';
+let secondCard = '';
+
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disable__card');
+
+    if (disabledCards.length ===20){
+        alert('Parabéns, vocé ganhou!!');
+    }
+}
+
+const checkCards = () => {
+    const firsCharacter = firstCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+
+    if (firsCharacter === secondCharacter) {
+
+        firstCard.firstChild.classList.add('disabled__card');
+        secondCard.firstChild.classList.add('disabled__card');
+
+        firstCard = '';
+        secondCard = '';
+
+        checkEndGame();
+
+    } else {
+
+        setTimeout(() => {
+            firstCard.classList.remove('reveal__card');
+            secondCard.classList.remove('reveal__card');
+
+            firstCard = '';
+            secondCard = '';
+
+        }, 500);
+
+
+    }
+
+}
+
+const revealCard = ({ target }) => {
+
+    if (target.parentNode.className.includes('reveal__card')) {
+        return;
+    }
+    if (firstCard === '') {
+        target.parentNode.classList.add('reveal__card');
+        firstCard = target.parentNode;
+    } else if (secondCard === '') {
+        target.parentNode.classList.add('reveal__card');
+        secondCard = target.parentNode;
+
+        checkCards();
+    }
+
+
 }
 
 const grid = document.querySelector('.grid');
@@ -28,22 +87,30 @@ const createCard = (character) => {
     const back = createElement('div', 'face back');
 
     front.style.backgroundImage = `url('../images/${character}.png')`;
-   
 
-    card.appendChild (front);
-    card.appendChild (back);
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    card.addEventListener('click', revealCard);
+    card.setAttribute('data-character', character);
+
 
     return card;
 
 }
 // esta criando a carta e
-const loadGame = () =>{
+const loadGame = () => {
 
-    const  duplicateCharacters = [ ...characters, ...characters];
+    const duplicateCharacters = [...characters, ...characters];
 
-    duplicateCharacters.forEach((character) =>{
+    const shuffleArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+
+
+    shuffleArray.forEach((character) => {
         const card = createCard(character);
         grid.appendChild(card);
+
     });
 }
 
